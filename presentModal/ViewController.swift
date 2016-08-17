8ed77bc37e51113635c0689ea78f739aff20910c
Paul Campbell <paulcampbell@pcampbellHLlaptop.local>
2016-08-17 14:12:16 -0500

    first commit

--- presentModal/ViewController.swift
+++ presentModal/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func openModal(sender: AnyObject) {
+        let nextController = UIImagePickerController()
+        self.presentViewController(nextController, animated: true, completion: nil)
+        
     }
-
 
 }
 
